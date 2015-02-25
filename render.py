64f7433402163a282eae8bfab74c5dda9ffadba1
Amy Ciavolino <aciavolino@etsy.com>
2015-02-24 23:15:20 -0500

    Put projects on seprate page, add instagram link, split up resume section

--- render.py
+++ render.py
@@ -14,30 +14,33 @@ ART_DIR = ASSET_DIR + '/art'
 DATA_FILE = lambda f: 'data/' + f
 SRC_FILE = lambda f: 'src/' + f
 
+PAGES = ['index', 'projects']
+
 def render():
-	projects = from_yaml(PROJECT_YAML)
-	data = {
+    projects = from_yaml(PROJECT_YAML)
+    data = {
         'assets': ASSET_DIR,
         'month': time.localtime().tm_mon,
         'year': time.localtime().tm_year,
-		'projects': projects[:NEW_PROJ_NUMBER],
-		'old_projects': projects[NEW_PROJ_NUMBER:],	
-		'resume': from_yaml(RESUME_YALM),
-		'art': art(),
+        'projects': projects[:NEW_PROJ_NUMBER],
+        'all_projects': projects,
+        'resume': from_yaml(RESUME_YALM),
+        'art': art(),
         'css': css()
-	}
-	tpl = open(SRC_FILE('homepage.mustache'), 'r').read()
-	rendered = pystache.render(tpl, data)
-	open('index.html', 'w').write(rendered)
+    }
+    for page_file in PAGES:
+        tpl = open(SRC_FILE(page_file + '.mustache'), 'r').read()
+        rendered = pystache.render(tpl, data)
+        open(page_file + '.html', 'w').write(rendered)
 
 def css():
     return compress(open(SRC_FILE('main.css'), 'r').read())
 
 def from_yaml(file):
-	return yaml.load(open(DATA_FILE(file),'r').read())
+    return yaml.load(open(DATA_FILE(file),'r').read())
 
 def art():
-	images = filter(lambda f: f != "small" and f[0] != '.', os.listdir(ART_DIR))
-	return [ {'image': i} for i in images ]
+    images = filter(lambda f: f != "small" and f[0] != '.', os.listdir(ART_DIR))
+    return [ {'image': i} for i in images ]
 
 render()
